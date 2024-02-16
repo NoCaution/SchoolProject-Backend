33d557f5fe37a91ab596366f183cafa03da24033
macbook <metehanakgul@ogr.trakya.edu.tr>
2024-02-16 10:29:19 +0300

    Security layer was added
    -JwtAuthenticationFilter and SecurityConfig was added to service

---
+++ place-service/src/main/java/com/edirnegezgini/placeservice/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.edirnegezgini.placeservice.config;
+
+import com.edirnegezgini.placeservice.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter authFilter;
+
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth.anyRequest().authenticated()
+                )
+                .sessionManagement(httpSecurity -> httpSecurity.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
---
+++ place-service/src/main/java/com/edirnegezgini/placeservice/security/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.edirnegezgini.placeservice.security;
+
+import com.edirnegezgini.commonservice.util.JWTUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("CustomAuthorization");
+
+
+        if(header == null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String[] headerValues = header.split(" ");
+
+        String token = headerValues[0];
+        String id = headerValues[1];
+        String password = headerValues[2];
+        String role = headerValues[3];
+
+       if(SecurityContextHolder.getContext().getAuthentication() == null){
+           User user = new User(
+                   id,
+                   password,
+                   Set.of(new SimpleGrantedAuthority(role))
+           );
+
+           if(jwtUtil.isTokenValid(token, user)){
+               UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                       user,
+                       null,
+                       user.getAuthorities()
+               );
+
+               authenticationToken.setDetails(
+                       new WebAuthenticationDetailsSource().buildDetails(request)
+               );
+
+               SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+           }
+
+           filterChain.doFilter(request, response);
+       }
+    }
+}
