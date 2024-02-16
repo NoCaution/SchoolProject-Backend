23cae1d7415026abd7c735c370ff4938a2b2ec2f
macbook <metehanakgul@ogr.trakya.edu.tr>
2024-02-16 10:21:55 +0300

    EnableJpaAuditing annotation was added to main method

--- visitation-service/src/main/java/com/edirnegezgini/visitationservice/VisitationServiceApplication.java
+++ visitation-service/src/main/java/com/edirnegezgini/visitationservice/VisitationServiceApplication.java
@@ -3,9 +3,11 @@ package com.edirnegezgini.visitationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableJpaAuditing
 public class VisitationServiceApplication {
 
 	public static void main(String[] args) {
