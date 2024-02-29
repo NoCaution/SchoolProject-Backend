fcf93a6f2bfd3809cdb266a5243a985568fa7581
macbook <metehanakgul@ogr.trakya.edu.tr>
2024-02-29 19:49:58 +0300

    JwtToken and CustomModelMapper beans were added to CommonServiceConfig in visitation-service

--- visitation-service/src/main/java/com/edirnegezgini/visitationservice/config/CommonServiceConfig.java
+++ visitation-service/src/main/java/com/edirnegezgini/visitationservice/config/CommonServiceConfig.java
@@ -2,7 +2,9 @@ package com.edirnegezgini.visitationservice.config;
 
 import com.edirnegezgini.commonservice.client.RestClient;
 import com.edirnegezgini.commonservice.service.CommonService;
+import com.edirnegezgini.commonservice.util.CustomModelMapper;
 import com.edirnegezgini.commonservice.util.JWTUtil;
+import com.edirnegezgini.commonservice.util.JwtToken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +23,15 @@ public class CommonServiceConfig {
     @Bean
     public RestClient restClient(){
         return new RestClient();
+    }
+
+    @Bean
+    public JwtToken jwtToken(){
+        return new JwtToken();
+    }
+
+    @Bean
+    public CustomModelMapper customModelMapper(){
+        return new CustomModelMapper();
     }
 }
